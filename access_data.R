c5f6afc6fa3289e741ae9318ceb8041ce569fc2e
Rohan Home Comp <rohan.daniscox@gmail.com>
2019-07-09 16:58:54 +1000

    Figured how to unlink data if needed

--- access_data.R
+++ access_data.R
@@ -13,6 +13,15 @@
   drop_auth()
   drop_dir()
   
-  drop_download("ilab2019/2017.feather")
+  drop_download("ilab2019/2017.feather",overwrite = TRUE)
   
   data_2017 <- read_feather("2017.feather")
+
+  
+  unlink("2017.feather")
+    
+  
+  ### Need to write a function to upload files to dropbox using drop_upload
+  
+  ### Need to write a function to delete all data files using unlink - or 
+  ### if the data itself is not costly which it may very well not be then leave it in there.
