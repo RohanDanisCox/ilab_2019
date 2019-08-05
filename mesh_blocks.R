27ff6b91d8a3994d48effd9c8748268bc888be52
Rohan Home Comp <rohan.daniscox@gmail.com>
2019-08-05 17:13:10 +1000

    Cleaned up code and generated green score

--- mesh_blocks.R
+++ mesh_blocks.R
@@ -1,21 +1,80 @@
 # Working with Mesh Blocks - this information could be useful for green spaces
 
-mesh_blocks <- read_csv("Data/ABS/Mesh Blocks Category.csv")
-mesh_blocks1 <- read_csv("Data/ABS/Mesh blocks to State Suburb.csv")
+# [0] ---- Load packages ----
 
-mesh_blocks2 <- mesh_blocks %>%
-  left_join(mesh_blocks1, by = c("MB_CODE_2016", "STATE_CODE_2016", "STATE_NAME_2016", "AREA_ALBERS_SQKM")) %>%
-  select(MB_CODE_2016,MB_CATEGORY_NAME_2016,SSC_NAME_2016,AREA_ALBERS_SQKM)
+  library(readr)
+  library(tidyverse)
+  library(rdrop2)
+  library(feather)
+  library(httpuv)
+  library(sp)
+  library(rgdal)
+  library(rgeos)
+  library(maptools)
+  library(ggmap)
+  library(broom)
+  library(sf)
 
-mesh_blocks3 <- mesh_blocks2 %>%
-  group_by(SSC_NAME_2016, MB_CATEGORY_NAME_2016) %>%
-  summarise(area = sum(AREA_ALBERS_SQKM)) 
+# [1] ---- Import mesh_block data ----
 
-mesh_blocks4 <- mesh_blocks3 %>%
-  group_by(SSC_NAME_2016) %>%
-  mutate(total_area = sum(area)) %>%
-  mutate(proportion = round((area/total_area),4))
+  mesh_block_category <- read_csv("Data/ABS/mesh_block_category.csv")
+  mesh_block_suburb <- read_csv("Data/ABS/mesh_block_suburb.csv")
+
   
+  table(mesh_block_category$GCCSA_NAME_2016)
+  
+  table(mesh_block_category$MB_CATEGORY_NAME_2016)
+# [2] ---- Calculate Green Score (Parkland & Include Primary Industries )
 
-haberfield <- mesh_blocks4 %>%
-  filter(SSC_NAME_2016 == "Haberfield")
+  mesh_block <- mesh_block_category %>%
+    left_join(mesh_block_suburb, by = c("MB_CODE_2016", "STATE_CODE_2016", "STATE_NAME_2016", "AREA_ALBERS_SQKM")) #%>%
+    #select(MB_CODE_2016,MB_CATEGORY_NAME_2016,SSC_NAME_2016,AREA_ALBERS_SQKM, SA2_NAME_2016,SA3_NAME_2016)
+  
+  sa2_mesh_block <- mesh_block %>%
+    group_by(SA2_NAME_2016) %>%
+    mutate(total_area_sa2 = sum(AREA_ALBERS_SQKM)) 
+  
+  sa2_mesh_block_1 <- sa2_mesh_block %>%
+    group_by(SA2_NAME_2016, MB_CATEGORY_NAME_2016, total_area_sa2) %>%
+    summarise(area_sa2 = sum(AREA_ALBERS_SQKM))
+  
+  sa2_mesh_block_2 <- sa2_mesh_block_1 %>%
+    filter(MB_CATEGORY_NAME_2016 == "Parkland" | MB_CATEGORY_NAME_2016 == "Primary Production") %>%
+    group_by(SA2_NAME_2016) %>%
+    mutate(green_area_sa2 = sum(area_sa2)) %>%
+    mutate(green_proportion_sa2 = green_area_sa2/total_area_sa2) %>%
+    select(SA2_NAME_2016,green_proportion_sa2) %>%
+    distinct()
+  
+  suburb_mesh_block <- mesh_block %>%
+    group_by(SSC_NAME_2016) %>%
+    mutate(total_area_suburb = sum(AREA_ALBERS_SQKM)) 
+  
+  suburb_mesh_block_1 <- suburb_mesh_block %>%
+    group_by(SSC_NAME_2016, MB_CATEGORY_NAME_2016, total_area_suburb) %>%
+    summarise(area_suburb = sum(AREA_ALBERS_SQKM))
+  
+  suburb_mesh_block_2 <- suburb_mesh_block_1 %>%
+    filter(MB_CATEGORY_NAME_2016 == "Parkland" | MB_CATEGORY_NAME_2016 == "Primary Production") %>%
+    group_by(SSC_NAME_2016) %>%
+    mutate(green_area_suburb = sum(area_suburb)) %>%
+    mutate(green_proportion_suburb = green_area_suburb/total_area_suburb) %>%
+    select(SSC_NAME_2016,green_proportion_suburb) %>%
+    distinct()
+  
+  mesh_block_1 <- mesh_block %>%
+    left_join(suburb_mesh_block_2) %>%
+    left_join(sa2_mesh_block_2) %>%
+    mutate(green_proportion_suburb = case_when(is.na(green_proportion_suburb) ~ 0,
+                                               TRUE ~ green_proportion_suburb),
+           green_proportion_sa2 = case_when(is.na(green_proportion_sa2) ~ 0,
+                                            TRUE ~ green_proportion_sa2)) %>%
+    mutate(green_score = green_proportion_suburb * 0.5 + green_proportion_sa2 * 0.5) %>%
+    mutate(green_score_decile = ntile(green_score,10))
+
+# [3] ---- Investigate scores
+  
+  mesh_block_check <- mesh_block_1 %>%
+    distinct(SSC_NAME_2016, .keep_all = TRUE) %>%
+    filter(GCCSA_NAME_2016 == "Greater Sydney")
+    
