9b929ba691f60929bd3137a274b5e2f68aff0f10
rohan_gcp <rohan.daniscox@gmail.com>
2019-10-22 10:12:53 +0000

    Fixing up the suburb investigator to be a lot cooler

--- suburb_investigation_app/app.R
+++ suburb_investigation_app/app.R
@@ -1,20 +1,17 @@
-
-# INVESTIGATIONS APP - useful video here: https://vimeo.com/rstudioinc/review/131218530/212d8a5a7a/#t=42m2s 
+# Suburb Investigator
 
 # [0] ---- Load packages ----
-library(shiny)
-library(sf)
-library(dplyr)
-library(ggplot2)
-library(shinycssloaders)
-library(markdown)
+    library(shiny)
+    library(sf)
+    library(dplyr)
+    library(ggplot2)
+    library(shinycssloaders)
+    library(markdown)
+    library(leaflet)
+    library(leafpop)
 
 # [1] ---- Load global data ----
     suburb_data <- readRDS("data/suburb_data.rds")
-    
-    sa3_data <- readRDS("data/sa3_data.rds")
-    
-    sa4_data <- readRDS("data/sa4_data.rds")
     
     nsw_data <- readRDS("data/nsw_data.rds")
     
@@ -36,10 +33,10 @@ library(markdown)
                      tabPanel("Investigator",
                               sidebarLayout(
                                   sidebarPanel(
-                                      selectizeInput("foo", label = "test",choices = NULL,
+                                      selectizeInput("suburb", label = "Search for up to 3 suburbs:",choices = NULL,
                                                      multiple = TRUE, options = list(maxItems = 3)),
                                       htmlOutput("variable_selector"),
-                                      withSpinner(plotOutput("plot_3", height = "320px"))
+                                      withSpinner(leafletOutput("plot_3", height = "320px"))
                                       ),
                                   # Show a plot of the generated distribution
                                   mainPanel(
@@ -60,99 +57,109 @@ library(markdown)
                                                              paging = FALSE,
                                                              info = FALSE))
         # trying selectize
-        updateSelectizeInput(session, "foo", choices = suburb_choice, server = TRUE)
+        updateSelectizeInput(session, "suburb", choices = suburb_choice, server = TRUE)
         
         # create reactive input values 
      
         output$variable_selector = renderUI({
             selectInput(inputId = "variable",
-                        label = "Variable:",
+                        label = "Choose a variable to inspect:",
                         choices = suburb_data %>% select(8:40) %>% names(),
                         selected = 1)
         })
         
         # get the reactive values which will be used in the plots
         
-        nsw_plot_data <- reactive({nsw_data})
-        
         suburb_plot_data <- reactive({
-            if(length(input$foo) == 1) {
-                suburb_data %>%filter(suburb_name == input$foo[[1]])
-            }
-            else if(length(input$foo) == 2) {
-                suburb_data %>%filter(suburb_name == input$foo[[1]] | suburb_name == input$foo[[2]])
-            }
-            else if(length(input$foo) == 3) {
-                suburb_data %>%filter(suburb_name == input$foo[[1]] | suburb_name == input$foo[[2]] | suburb_name == input$foo[[3]])
-            }
-            })
-        
-        sa3_plot_data <- reactive({sa3_data %>%
-                filter(sa3_name == input$sa3)
-            })
-        
-        sa4_plot_data <- reactive({sa4_data %>%
-                filter(sa4_name == input$sa4)
+            suburb_data %>%filter(suburb_name %in% input$suburb)
             })
         
         # create the plots
         first_plot <- reactive({
-            if(length(input$foo) < 1) {
+            if(length(input$suburb) < 1) {
                 ggplot(nsw_data,aes(x = year, y = !!as.symbol(input$variable))) + 
-                    geom_line(linetype = "dashed") + 
-                    labs(x = "Year", y = as.character(input$variable))
+                    geom_line(linetype = "dashed", colour = "black", size = 1) + 
+                    labs(x = "Year", y = as.character(input$variable),title = paste0(input$variable," in NSW"))
             }
-            else if(length(input$foo) >= 1){
-                ggplot(nsw_data,aes(x = year, y = !!as.symbol(input$variable))) + 
-                    geom_line(linetype = "dashed") + 
+            else if(length(input$suburb) >= 1){
+                ggplot(nsw_data,aes(x = year, y = !!as.symbol(input$variable), colour = "NSW")) + 
+                    geom_line(linetype = "dashed", colour = "black", size = 1) + 
                     geom_line(data = suburb_plot_data(),
-                              mapping = aes(x = year,y = !!as.symbol(input$variable), colour = suburb_name)) +
+                              mapping = aes(x = year,y = !!as.symbol(input$variable), colour = suburb_name), size = 1) +
                     theme_minimal(base_size = 16) +
                     #scale_color_manual(name = "Geography",
                     #breaks = c("black","blue","darkgreen","red"),
                     #labels = c("Suburb","SA3","SA4","NSW"),
                     #guide = "legend") + 
-                    labs(x = "Year", y = as.character(input$variable))
+                    labs(x = "Year", y = as.character(input$variable), colour = "Suburb",
+                         title = paste0(input$variable," in NSW"))
                 }
             })
         
         second_plot <- reactive({
-            if(length(input$foo) < 1) {
+            if(length(input$suburb) < 1) {
                 ggplot(suburb_data,aes(!!as.symbol(input$variable))) +
                     geom_density() +
                     theme_minimal(base_size = 16)
             }
-            else if(length(input$foo) >= 1){
+            else if(length(input$suburb) >= 1){
                 ggplot(suburb_data,aes(!!as.symbol(input$variable))) +
                     geom_density() +
                     geom_vline(data = suburb_plot_data(),
-                               aes(xintercept = !!as.symbol(input$variable)), 
-                               color = "#FC4E08", linetype = "dashed", size = 1)+
+                               aes(xintercept = !!as.symbol(input$variable), colour = suburb_name), 
+                               linetype = "dashed", size = 1, alpha = 0.5)+
                     theme_minimal(base_size = 16)
             }
         })
         
-
         output$plot_1 <- renderPlot({
             first_plot()
-            })
-            
+        })
+        
         output$plot_2 <- renderPlot({
             second_plot()
         })
         
-        output$plot_3 <- renderPlot({
-            
+        output$plot_3 <- renderLeaflet({
+            leaflet(map,options = leafletOptions(minZoom = 6)) %>%
+                addTiles() %>%
+                setView(148.9211,-32.2532, zoom = 6)
+        })
+        
+        observeEvent(input$suburb,{
             map_subset <- map %>%
-                filter(sa4_name == input$sa4) %>%
-                mutate(fill = case_when(suburb_name == input$suburb ~ "suburb",
-                                          suburb_name != input$suburb & sa3_name == input$sa3 ~ "sa3",
-                                          TRUE ~ "No"))
+                filter(suburb_name %in% input$suburb)
             
-            ggplot() +
-                geom_sf(data = map_subset, aes(fill = fill)) +
-                scale_fill_manual(values = c("white","pink","red"), guide = FALSE)
+            if(length(input$suburb == 1)) {
+                top <- map_subset %>%
+                    filter(suburb_name == input$suburb[[1]]) %>%
+                    st_centroid(geometry)
+            }
+            else if(length(input$suburb == 2)) {
+                top <- map_subset %>%
+                    filter(suburb_name == input$suburb[[2]]) %>%
+                    st_centroid(geometry)
+            }
+            else if(length(input$suburb == 3)) {
+                top <- map_subset %>%
+                    filter(suburb_name == input$suburb[[3]]) %>%
+                    st_centroid(geometry)
+            }     
+            top <- map_subset %>%
+                st_centroid(geometry)
             
+            top_lat <- top$geometry[[1]][1]
+            top_lng <- top$geometry[[1]][2]
+            
+            leafletProxy("plot_3") %>%
+                clearShapes() %>%
+                flyTo(top,lng = top_lat,lat = top_lng,zoom = 10) %>%
+                addPolygons(data = map_subset,
+                            weight = 1, 
+                            fillColor = "red", 
+                            color = "black",
+                            opacity = 1,
+                            fillOpacity = 0.8)
         })
     }
 
